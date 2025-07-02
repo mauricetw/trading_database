83ffe9ece78f5de415a3714c8b356971533894d2
Maurice <61717673+mauricetw@users.noreply.github.com>
2025-07-02 16:08:24 +0800

    vup y/

---
+++ routers/user.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database.db import get_db
+from schemas.user_schema import UserResponse
+from models.user import User
+
+router = APIRouter()
+
+@router.get("/users/{user_id}", response_model=UserResponse)
+def get_user(user_id: str, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"user": user}
