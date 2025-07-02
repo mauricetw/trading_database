7074521704817dd3435c5a639a3df5b9c77e327b
Maurice <61717673+mauricetw@users.noreply.github.com>
2025-07-02 13:38:00 +0800

    修改成任意IP皆可連線

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from routers import auth, products, orders
 from database.db import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# 加上這段以允許任何來源（網域）都能連線
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 不限制來源
+    allow_credentials=True,
+    allow_methods=["*"],  # 允許所有方法（GET、POST 等）
+    allow_headers=["*"],  # 允許所有 headers
+)
 
 # 建立資料庫表
 Base.metadata.create_all(bind=engine)
